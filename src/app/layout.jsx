317045990e2cfafa716bf7717676e993b31d42f0
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-07-22 14:27:22 +0200

    add: Vercel Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { RootLayout } from '@/components/RootLayout'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/tailwind.css'
 import '@/styles/satoshi.css'
@@ -15,6 +16,7 @@ export default function Layout({ children }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <Analytics />
       </body>
     </html>
   )
